bb6910bd0b200178c631c35acc4ec998913be82a
Novendhi Reiner Maturbongs <55633788+novendhireiner@users.noreply.github.com>
2024-05-16 14:29:20 +0200

    Update Python Training 8 - Syntetischer Datensatz.py

--- Python Training 8 - Syntetischer Datensatz.py
+++ Python Training 8 - Syntetischer Datensatz.py
@@ -1,17 +1,3 @@
-# Dieses Skript verwendet die Python-Bibliothek Faker, um synthetische Nutzerdaten zu generieren und sie in einem 
-# Pandas DataFrame zu organisieren. Zuerst wird der Faker initialisiert, der für die Generierung von Namen, Adressen und 
-# geografischen Koordinaten verwendet wird. Die Funktion generate_user_data() wird definiert, um zufällige Nutzerdaten 
-# zu erstellen, einschließlich einer Benutzer-ID, einem Namen, einer Adresse und einer geografischen Lage.
-#
-# Anschließend wird eine Liste von 10 Einträgen generiert, indem die generate_user_data()-Funktion wiederholt aufgerufen wird. 
-# Jeder Eintrag enthält eine eindeutige Benutzer-ID, einen zufälligen Namen, eine Adresse und eine geografische Lage.
-# Diese Einträge werden dann in einem Pandas DataFrame organisiert, wobei die Spalten entsprechend den Nutzerdaten 
-# benannt werden. Schließlich wird der erstellte DataFrame ausgegeben, um die synthetischen Nutzerdaten anzuzeigen.
-#
-# Dieses Skript ermöglicht die schnelle Erzeugung von synthetischen Nutzerdaten für Testzwecke oder Demonstrationszwecke 
-# und zeigt, wie Python-Bibliotheken wie Faker und Pandas effektiv zusammenarbeiten können, um Daten zu generieren und 
-# zu organisieren.
-
 import pandas as pd
 import random
 from faker import Faker
