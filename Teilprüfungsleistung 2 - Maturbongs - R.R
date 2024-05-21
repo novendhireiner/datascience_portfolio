78bdfcfa6ebfca96b57a52d85a47193e34c59fec
Novendhi Reiner Maturbongs <55633788+novendhireiner@users.noreply.github.com>
2024-05-21 14:17:41 +0200

    Add files via upload

---
+++ Teilprüfungsleistung 2 - Maturbongs - R.R
@@ -0,0 +1,39 @@
+#Aufgabe A:
+#Scatterplot:
+library(ggplot2)
+
+data(airquality)
+
+ggplot(airquality, aes(x = Temp, y = Ozone, color = factor(Month))) +
+  geom_point() +
+  labs(x = "Temperatur", y = "Ozonkonzentration", color = "Monat") +
+  scale_color_discrete(name = "Monat")+
+  ggtitle("Luftqualitätsvergleich zw. Temperatur und Ozonkonzentration")
+
+#Interpretation:
+#je höher die Temperatur ist, desto höhere die Ozonkonzentration ist. Es wird je nach Monat variiert.
+
+#Aufgabe B:
+#Balkendiagramm:
+#den Durchschnitt der Ozonkonzentration pro Monat
+average_ozone <- aggregate(Ozone ~ Month, data = airquality, FUN = mean)
+
+#Balkendiagramm:
+ggplot(average_ozone, aes(x = factor(Month), y = Ozone)) +
+  geom_bar(stat = "identity", fill = "blue") +
+  labs(x = "Monat", y = "Durchschnittliche Ozonkonzentration")+
+  ggtitle("Balkendiagramm: Durchschnittliche Ozonkonzentration")
+
+#Interpretation:
+#Die durchschnittliche Ozonkonzentration im Monat 7 und 8 tendeziell höher als andere Monat.
+
+
+#Aufgabe C:
+#Boxplot:
+ggplot(airquality, aes(x = factor(Month), y = Temp)) +
+  geom_boxplot(fill = "blue") +
+  labs(x = "Monat", y = "Temperatur") +
+  ggtitle("Boxplot: Temperatur für jeden Monat")
+
+#Interpretation:
+#Temperaturwerte variieren im Monat 5 bis 9 je nach Kastenhöhe. Außreiser befinden sich im Monat 6 und 7.
