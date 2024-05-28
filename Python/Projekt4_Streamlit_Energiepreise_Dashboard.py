e960c7d4c5eeeb154c06947b77ebb9e6ba28fcd8
Novendhi Reiner Maturbongs <55633788+novendhireiner@users.noreply.github.com>
2024-05-28 11:45:07 +0200

    Update DF Path

--- Python/Projekt4_Streamlit_Energiepreise_Dashboard.py
+++ Python/Projekt4_Streamlit_Energiepreise_Dashboard.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import streamlit as st
 
 # Daten Aufbereitung
-data = pd.read_excel("Energiepreise.xlsx")
+data = pd.read_excel("Python/Energiepreise.xlsx")
 
 # Streamlit-Anwendung starten
 st.title("Energiepreise an private Haushalt von 2008 bis 2023 Benchmarking Dashboard")
